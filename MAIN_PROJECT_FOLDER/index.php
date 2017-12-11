d199f7619d1cad01fb7d354fb0ee81c4f42f11fa
Philipp <Philipp98yt@gmail.com>
2017-12-11 03:01:20 +0300

    1
    1

--- MAIN_PROJECT_FOLDER/index.php
+++ MAIN_PROJECT_FOLDER/index.php
@@ -208,7 +208,27 @@
 				<div class="col-md-4">
 					
 				</div>
+				<div class="col-md-4 justify-content-end">
+					<ul>
+						<li>onetwo@mail.com</li>
+						<li>+79819656366</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		</div>
+	</footer>
+
+
+
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+>>>>>>> sveta
 </body>
 
 </html>
