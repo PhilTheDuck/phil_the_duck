1dd5851ab25aa4a73eac41ff19259515c208eafb
Philipp <Philipp98yt@gmail.com>
2017-12-11 03:06:12 +0300

    Functional
    Phil made it

--- MAIN_PROJECT_FOLDER/index.php
+++ MAIN_PROJECT_FOLDER/index.php
@@ -3,8 +3,7 @@
 
 
 <head>
-<<<<<<< HEAD
-    <title>кк</title>
+    <title>FindYourMate</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -12,7 +11,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.css">
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
-=======
+
 	<title>FindYourMate</title>
 	<!-- Required meta tags -->
 	<meta charset="utf-8">
@@ -21,88 +20,14 @@
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="css/bootstrap.css">
 	<link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
->>>>>>> sveta
 
 </head>
 
 <body>
-<<<<<<< HEAD
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container">
-                <a class="navbar-brand" href="#">
-                    <img src="img/1.jpg" width="30" height="30" alt="">
-                </a>
-                <div class="nav justify-content-end">
-                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Регистрация</a>
-                        </li>
-                    </ul>
-                    <form class="form-inline my-2 my-lg-0">
-                        <input class="form-control mr-sm-2" type="text" placeholder="Логин" name="login">
-                        <input class="form-control mr-sm-2" type="password" placeholder="Пароль" name="password">
-                        <button class="btn btn-outline-success" type="submit">Войти</button>
-                    </form>
-                </div>
-            </div>
-        </nav>
-        <div class="top-img">
-
-        </div>
-    </header>
-    <div name="spinner">
-       <h2 align="center">You must consider, BUT!!! choose wizапавапапапрапаely...</h2>
-        <style>
-            img.rot {
-                animation: 1s linear 0s normal none infinite running rot;
-                -webkit-animation: 1s linear 0s normal none infinite running rot;
-                width: 200px;
-                margin-left: 50px
-            }
-            
-            @keyframes rot {
-                0% {
-                    transform: rotate(0deg);
-                }
-                100% {
-                    transform: rotate(360deg);
-                }
-            }
-            
-            @-webkit-keyframes rot {
-                0% {
-                    transform: rotate(0deg);
-                }
-                100% {
-                    transform: rotate(360deg);
-                }
-            }
-
-        </style>
-        <img class="rot" src="img/s1.jpg">
-        <img class="rot" src="img/q1.jpg">
-        <img class="rot" src="img/q2.jpg">
-        <img class="rot" src="img/q3.jpg">
-        <img class="rot" src="img/q4.png">
-    </div>
-
-
-
-
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-=======
 	<header>
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
 			<div class="container">
 				<div class="nav justify-content-start">
-
 					<a class="navbar-brand" href="#">
 						<img src="img/1.png" width="248" height="173" alt="">
 					</a>
@@ -121,7 +46,7 @@
 				<div class="nav justify-content-end">
 					<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
 						<li class="nav-item">
-							<a class="nav-link" href="#">Регистрация</a>
+							<a class="nav-link" href="registration.html">Регистрация</a>
 						</li>
 					</ul>
 					<form class="form-inline my-2 my-lg-0" action="login.php" method="post">
@@ -228,7 +153,6 @@
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
->>>>>>> sveta
 </body>
 
 </html>
